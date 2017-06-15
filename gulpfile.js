f0db4c41dff8e9459990be4b5662f372a64eff24
alboz1 <vucajjuerd@gmail.com>
2017-06-15 03:51:09 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const server = require('gulp-webserver');
+
+gulp.task('server', () => {
+    return gulp.src('./')
+        .pipe(server({
+            livereload: true,
+            open: true,
+            port: 8080
+        }));
+});
+
+gulp.task('sass', () => {
+    return gulp.src('sass/main.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', () =>{
+    gulp.watch('sass/*.sass', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch', 'server']);
