be70e2ac606bc3394ea3278898da30d4cba3de89
alboz1 <vucajjuerd@gmail.com>
2019-12-18 16:21:37 +0100

    removed unused function

--- js/index.js
+++ js/index.js
@@ -184,14 +184,3 @@ const view = {
     }
 };
 app.init();
-
-function convertUTCDateToLocalDate(date) {
-    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
-
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
-
-    newDate.setHours(hours - offset);
-
-    return newDate;
-}
